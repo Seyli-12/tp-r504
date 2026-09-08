1ccaab54254eafe5667c93afe273c7057591ef7c
seyli-12 <keb.ilyes12@gmail.com>
2026-09-08 17:14:44 +0200

    modif

--- TP1/fonctions.py
+++ TP1/fonctions.py
@@ -2,7 +2,7 @@
 
 
 def puissance(a,b):
-    resultat = 0 
+    resultat = 1 
     print("Le type de la premiere valeur est : ",type(a))
     print("Le type de la deuxieme valeur est : ",type(b))
     if not type(a) is int :
@@ -13,7 +13,7 @@ def puissance(a,b):
     if a == 0 and b < 0 :
           raise ValueError("Opération indéfinie pour un nombre négatif")
     
-    for i in range(b): 
-        resultat += a
+    for i in range(abs(b)): 
+        resultat *= a
     return resultat
 
