0112d5a59a57928cd8a45d70b5eeb6129d8bef7f
seyli-12 <keb.ilyes12@gmail.com>
2026-09-08 16:11:42 +0200

    fichier python

---
+++ TP1/fonctions.py
@@ -0,0 +1,13 @@
+
+
+
+def puissance(a,b):
+    print("Le type de la premiere valeur est : ",type(a))
+    print("Le type de la deuxieme valeur est : ",type(b))
+    if not type(a) is int :
+          raise TypeError("Only integers are allowed")
+    if not type(b) is int :
+          raise TypeError("Only integers are allowed")
+    resultat = a**b
+    return resultat
+
--- TP1/prog1.py
+++ TP1/prog1.py
@@ -1,2 +1,21 @@
 print("Hello, World!")
+import fonctions as f
+
+print(type(2))
+
+while True :
+     
+     val1 = input("val a ?\n")
+     if "." in val1 or "," in val1:
+        raise ValueError("Pas de float autorisé !")
+     val1 = int(val1)
+
+     val2 = input("val b ?\n")
+     if "." in val2 or "," in val2:
+        raise ValueError("Pas de float autorisé !")
+     val2 = int(val2)
+    
+
+     resultat = f.puissance(val1,val2)
+     print(resultat)
 
